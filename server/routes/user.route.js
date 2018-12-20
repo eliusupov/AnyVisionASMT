61b99574dc5a75c6838cc913c7d7af5d3a6ff72a
Eli Usupov <eliusupov123@gmail.com>
2018-12-20 10:38:52 +0200

    fix3

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Require the controllers WHICH WE DID NOT CREATE YET!!
 const user_controller = require('../controllers/user.controller');
 
-
-// a simple test url to check that all of our files are communicating correctly.
 router.post('/create', user_controller.user_create);
 router.post('/login', user_controller.user_login);
 router.get('/get/all', user_controller.user_get_all);
